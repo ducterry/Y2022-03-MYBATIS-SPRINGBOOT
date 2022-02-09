a3edf0b39240d814711468831e7a44138b14fec0
Đức Nguyễn Đăng <duc.nd5@shbfinance.com.vn>
2022-02-09 09:33:07 +0700

    Inital Commit

---
+++ src/main/java/com/ndangducbn/startspringboot/MyBatisBootApplication.java
@@ -0,0 +1,13 @@
+package com.ndangducbn.startspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisBootApplication.class, args);
+    }
+
+}
